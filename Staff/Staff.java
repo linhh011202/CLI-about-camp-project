b5e5a0534f72f223ecaa3cb85b2ebeb662fad4ec
kingfishersx <131509908+kingfishersx@users.noreply.github.com>
2023-11-04 19:14:49 +0800

    Update Staff.java

--- Staff/Staff.java
+++ Staff/Staff.java
@@ -2,15 +2,22 @@ package staff;
 
 import java.util.ArrayList;
 
-public class Staff 
+public class Staff extends User
 {
     private ArrayList<Camp> camps; //seems like a bad idea to directly composition the camps to the staff with nothing in between though.
     private CampOperator campOperator;
     private SuggestionOperator suggestionOperator;
     private EnquiryOperator enquiryOperator;
-
+    
     public Staff()
     {
+        this.campOperator=new CampOperator(this);
+        this.suggestionOperator=new SuggestionOperator(this);
+        this.enquiryOperator= new EnquiryOperator(this);
+    }
+    public Staff(String name, String password, String facultyInformation) {
+        super(String name, String password, String facultyInformation);
+        this.camps = new ArrayList<>();
         this.campOperator=new CampOperator(this);
         this.suggestionOperator=new SuggestionOperator(this);
         this.enquiryOperator= new EnquiryOperator(this);
@@ -20,4 +27,5 @@ public class Staff
     {
         return this.camps;
     }
+
 }
