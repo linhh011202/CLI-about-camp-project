719f45eb7e79a6b4912c4e9527f40eaf801653c6
Cristina <linh012@e.ntu.edu.sg>
2023-11-08 23:36:27 +0800

    Add src

---
+++ src/FileLoader.java
@@ -0,0 +1,107 @@
+
+// We need a Class to load the file, this comes from the Java library
+// Search online to see what library
+import java.io.FileReader;
+import java.io.BufferedReader;
+// We need this to handle if the file is not found
+import java.io.IOException;
+import java.util.ArrayList;
+
+import user.User;
+import user.Staff;
+import user.Student;
+
+public class FileLoader {
+    public static final String STUDENT_PATH = "./student_list.csv"; // Define where to load the file
+    public static final String STAFF_PATH = "./staff_list.csv";
+
+    // This thing is Javadoc!
+    /***
+     * This function read the content of the .csv file and parse into the appropriate data structure (Student List/ Staff List)
+     * We make sure that the file reading functionality works before we try to add into more complex class structure
+     * @param path is the path to the file
+     * @return nothing because it is a void
+     */
+    public static void readFile(String path){
+
+        try{ // Try, catch structure to handle exception
+            BufferedReader reader = new BufferedReader(new FileReader(path)); // Create a BufferedReader object
+            String line = reader.readLine(); // This will give us the line string
+            int count = 0;
+            while (line != null) {
+                if (count != 0){   // This logic remove the need for us to delete the first line. Just keep track of the count as we go down the line
+                    if (line.isEmpty()) { // This will skip the empty line
+                        continue;
+                    }
+                    //We want to split the line string into sub components
+                    String[] lineComponents = line.split(","); // This will return an array of string
+                    
+                    // Since we have all the different components, let's print them in a different manner
+                    System.out.print("Name: ");
+                    System.out.print(lineComponents[0]);
+                    System.out.print("\tEmail: ");
+                    System.out.print(lineComponents[1]);
+                    System.out.print("\tFaculty: "); // \t is a tab 
+                    System.out.println(lineComponents[2]); // println will add a newline at the end. print will not add a newline
+                    // System.out.println(line); // Comment this out to see the new printing
+                }   // SO now the different components are nicely printed!
+
+                count++; // Increment the count after reading the line
+                line = reader.readLine();
+            }
+            
+            reader.close();
+        } catch (IOException e){
+            System.out.println("File not found");
+        }
+
+    }
+
+
+    // Remember to do javadoc as you code so that you can understand, and it is also one of the requirement
+    // Start by typing / follow by 3 *** and it will auto generate
+
+    /***
+     * This function load the file into the appropriate data structure
+     * @param path file path
+     * @param userList the appropriate list
+     */
+
+    public static void loadFile(String path, String type, ArrayList<User> userList){
+        // This demonstrate the concept of POLYMORPHISM. I pass in the User[] Array because I don't know what to expect
+        // But in the Main.java, I will pass the appropirate Student or Staff list, but they are children of User so it is valid
+        // Now, copy and paste the parsing of the file
+
+
+        try{ // Try, catch structure to handle exception
+            BufferedReader reader = new BufferedReader(new FileReader(path)); // Create a BufferedReader object
+            String line = reader.readLine(); // This will give us the line string
+            while (line != null) {
+                    if (!(line.isEmpty())) { 
+                    String[] lineComponents = line.split(","); // This will return an array of string
+                    
+                    if (type == "staff"){
+                        Staff staff = new Staff(lineComponents[0], lineComponents[1], lineComponents[2]); // So here we need a constructor
+                        // Here we add to the list!
+                        userList.add(staff); // The array list allows us to quickly add elements!
+                    }
+                    else if (type == "student"){
+                        Student student = new Student(lineComponents[0], lineComponents[1], lineComponents[2]); // So here we need a constructor
+                        userList.add(student);
+                    }
+                    else{
+                        System.out.println("Invalid type");
+                    }}
+                line = reader.readLine();
+            }  // Always check your logic. Especially the parsing of the string
+            reader.close();
+        } catch (IOException e){
+            System.out.println("File not found");
+        }
+
+
+    }
+
+
+    }
+
---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import user.User;
+
+import java.io.File;
+import java.util.Scanner;
+
+import FileLoader;
+import user.DataList;// Import DataList definition
+
+public class Main {
+
+
+    // Define the instance of the DataList as this will be used by the Main program
+
+
+    public static void main(String[] args) {  // THIS IS THE ENTRY POINT OF THE PROGRAM
+        System.out.println("Start program"); // This is a print statement
+        DataList dataList = new DataList();
+
+        // FileLoader.readFile(FileLoader.STUDENT_PATH); This is just a dummy function to check 
+        // We want to load the data in the file into the list, so it might go something like this
+        FileLoader.loadFile(FileLoader.STUDENT_PATH, "student", dataList.studentList);// We want to load the student file into the student list
+        FileLoader.loadFile(FileLoader.STAFF_PATH, "staff", dataList.staffList);// We want to load the staff file into the staff list
+        
+        // It says that we cannot pass ArrayList<Student> into ArrayList<User>, but polymorphishm says it should right?
+        // Let's run to find out
+        // Ok so it doesn't work... SO where does Polymorphism work?
+        System.out.println("Student List Size: " + dataList.studentList.size());
+        System.out.println("Staff List Size: "+ dataList.staffList.size());
+
+        System.out.println("First Student Name" + dataList.studentList.get(0).name); 
+        // Get the first element of the student list, and get the name of that student
+        // Oh no.. it get ugly again... Might as well just edit the student list....
+
+        
+        System.out.println("Finish Loading Files"); // This is a print statement
+
+
+        // //while loop to ask user if they want to exit the program, write exit to exit
+        // Scanner scanner = new Scanner(System.in);
+        // String input = "";
+        // while (!input.equals("exit")) {
+        //     System.out.println("Enter a command: ");
+        //     input = scanner.nextLine();
+        //     System.out.println("You entered: " + input);
+        // }
+    
+
+
+    }
+}
+
+
