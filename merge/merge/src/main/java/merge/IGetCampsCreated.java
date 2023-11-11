006c44251ba9b62f5271a3144bf4d1b77081d582
BuggerBugs <sqiyang18@gmail.com>
2023-11-11 17:44:41 +0800

    Add interface for kx to use

---
+++ merge/merge/src/main/java/merge/IGetCampsCreated.java
@@ -0,0 +1,8 @@
+package merge;
+
+import java.util.ArrayList;
+
+public interface IGetCampsCreated 
+{
+    public ArrayList<String> getListCampStaffCreated(String staffName);
+}
---
+++ merge/merge/src/main/java/merge/ListCampsStaffCreatedGetter.java
@@ -0,0 +1,29 @@
+package merge;
+
+import java.util.ArrayList;
+
+public class ListCampsStaffCreatedGetter implements IGetCampsCreated
+{
+    private CampDataBase campDataBase;
+
+    public ListCampsStaffCreatedGetter(CampDataBase campDataBase)
+    {
+        this.campDataBase=campDataBase;
+    }
+    public ArrayList<String> getListCampStaffCreated(String staffName)
+    {
+        ArrayList<Camp> allCamps=campDataBase.getAllCamps();
+        ArrayList<String> campsStaffCreated=new ArrayList<String>(0);
+
+        for(int i=0;i<allCamps.size();++i)
+        {
+            if(allCamps.get(i).getStaffInCharge().equals(staffName))
+            {
+                campsStaffCreated.add(allCamps.get(i).getCampName());
+            }
+        }
+        return campsStaffCreated;
+    }
+
+
+}
