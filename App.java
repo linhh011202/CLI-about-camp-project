79e2591e1262262e4ee1fc7cae7d466493616793
jeengg <jeengyee@ymail.com>
2023-11-12 01:28:44 +0800

    figured out packages

---
+++ App.java
@@ -0,0 +1,8 @@
+import student.*;
+
+public class App {
+    public static void main(String[] args) {
+        Student s1 = new Student("tomn", 34);
+        System.out.println(s1.getAge());
+    }
+}
