6387852c23998d6beb4100b37808f5129af9b241
jeengg <jeengyee@ymail.com>
2023-11-15 11:04:29 +0800

    split into packages

--- App.java
+++
@@ -1,8 +0,0 @@
-import student.*;
-
-public class App {
-    public static void main(String[] args) {
-        Student s1 = new Student("tomn", 34);
-        System.out.println(s1.getAge());
-    }
-}
--- program/program/src/main/java/program/Main.java
+++ program/program/src/main/java/program/Main.java
@@ -1,4 +1,4 @@
-package test;
+package program;
 
 public class Main {
     public static void main(String[] args) {
