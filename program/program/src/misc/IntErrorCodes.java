fe59e3d22e1ff7a0e484541f0f7a1214141f3487
jeengg <jeengyee@ymail.com>
2023-11-15 14:26:39 +0800

    doneee

--- program/program/src/misc/IntErrorCodes.java
+++ program/program/src/misc/IntErrorCodes.java
@@ -9,7 +9,7 @@ import user.*;
 //NOT IN UMLYET
 
 public class IntErrorCodes {
-    static final int CAMP_NOT_FOUND = -1;
-    static final int INSUFFICIENT_STUDENT_SLOTS = -2;
-    static final int INSUFFICIENT_COMMITTEE_SLOTS = -3;
+    public static final int CAMP_NOT_FOUND = -1;
+    public static final int INSUFFICIENT_STUDENT_SLOTS = -2;
+    public static final int INSUFFICIENT_COMMITTEE_SLOTS = -3;
 }
