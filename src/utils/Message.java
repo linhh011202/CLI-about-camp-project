e1f80c9816a566cfe5665b77d4d20233da4058a7
Bugger <96870890+BuggerBugs@users.noreply.github.com>
2023-11-15 16:36:43 +0800

    Delete src directory

--- src/utils/Message.java
+++
@@ -1,6 +0,0 @@
-package utils;
-
-public class Message {
-    public static final String HELLO = "Welcome to the application";
-    // HELLO is the variable name. it's uppercase because it's a constant(convention rule)
-}
